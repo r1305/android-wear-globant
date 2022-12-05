26aae12f855e7cb15585d4f4067d824624b937ea
Rogger Aburto <rogger.aburto@gmail.com>
2022-12-05 17:58:23 -0500

    list and notifications

--- wear/src/main/java/com/example/mywearosapplication/MainActivity.kt
+++ wear/src/main/java/com/example/mywearosapplication/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.mywearosapplication
 
-import android.app.Activity
+import android.R
+import android.app.*
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.mywearosapplication.databinding.ActivityMainBinding
+import kotlin.random.Random
+
 
 class MainActivity : Activity() {
 
@@ -18,11 +25,49 @@ class MainActivity : Activity() {
         binding.btnCheck.setOnClickListener {
             Toast.makeText(this, "Confirmado", Toast.LENGTH_SHORT).show()
             binding.txtTitle.text = "Confirmado"
+            sendNotification()
+        }
+
+        binding.btnList.setOnClickListener {
+            startActivity(Intent(this, RecycleActivity::class.java))
         }
 
         binding.btnClose.setOnClickListener {
             Toast.makeText(this, "Denegado", Toast.LENGTH_SHORT).show()
             binding.txtTitle.text = "Denegado"
         }
+    }
+
+    private fun sendNotification() {
+        val random = Random.nextInt(0,100)
+        val group_key = "group_messages"
+        val group_channel = "12345"
+        val message = "This is an example with random number: $random"
+        val notification_id = random
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val notificationChannel =
+            NotificationChannel(group_channel, group_key, importance)
+        notificationChannel.enableLights(true)
+        notificationChannel.lightColor = Color.RED
+        notificationChannel.enableVibration(true)
+        notificationChannel.vibrationPattern =
+            longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, group_channel)
+            .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setContentTitle(title)
+            .setContentText(message)
+
+        val resultIntent = Intent(this, MainActivity::class.java)
+        val stackBuilder: TaskStackBuilder = TaskStackBuilder.create(this)
+        stackBuilder.addParentStack(MainActivity::class.java)
+        stackBuilder.addNextIntent(resultIntent)
+        val resultPendingIntent: PendingIntent =
+            stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        builder.setContentIntent(resultPendingIntent)
+        notificationManager.notify(notification_id, builder.build())
     }
 }
---
+++ wear/src/main/java/com/example/mywearosapplication/RecycleActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mywearosapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.wear.widget.WearableLinearLayoutManager
+import androidx.wear.widget.WearableRecyclerView
+private const val MAX_ICON_PROGRESS = 0.65f
+
+class RecycleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycle)
+
+        val wearableRecycler = findViewById<WearableRecyclerView>(R.id.recycler_launcher_view)
+        wearableRecycler.apply {
+            isEdgeItemsCenteringEnabled = true
+            layoutManager = WearableLinearLayoutManager(this@RecycleActivity, CustomScrollingLayoutCallback())
+        }
+    }
+}
+
+class CustomScrollingLayoutCallback : WearableLinearLayoutManager.LayoutCallback() {
+
+    private var progressToCenter: Float = 0f
+
+    override fun onLayoutFinished(child: View, parent: RecyclerView) {
+        child.apply {
+            // Figure out % progress from top to bottom
+            val centerOffset = height.toFloat() / 2.0f / parent.height.toFloat()
+            val yRelativeToCenterOffset = y / parent.height + centerOffset
+
+            // Normalize for center
+            progressToCenter = Math.abs(0.5f - yRelativeToCenterOffset)
+            // Adjust to the maximum scale
+            progressToCenter = Math.min(progressToCenter, MAX_ICON_PROGRESS)
+
+            scaleX = 1 - progressToCenter
+            scaleY = 1 - progressToCenter
+        }
+    }
+}
